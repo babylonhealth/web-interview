19fef219c6a16d19b0a3d5dc77b31257330fdd95
PaulPineda <6228978+PaulPineda@users.noreply.github.com>
2019-01-07 22:22:41 +0000

    * add react-router * scaffold out routes for Home, Appointments and Book appointments screens * basic test still passes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import logo from './logo.png'
 import { API_ENDPOINT } from './config'
 
 import './App.scss'
 
+import Home from './pages/Home'
+import Appointments from './pages/Appointments'
+import BookAppointment from './pages/BookAppointment'
+
 class App extends Component {
   componentDidMount() {
     fetch(`${API_ENDPOINT}/users/1`)
       .then(res => res.json())
-      .then(() => {
+      .then(res => {
         // TODO: Handle response here
+        console.log(res)
       })
       .catch(() => {
         // TODO: Handle error here
@@ -19,12 +24,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <header className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-        </header>
-        <h1>This is where your code goes!</h1>
-      </div>
+      <Router>
+        <div className="app">
+          <header className="app-header">
+            <img src={logo} className="app-logo" alt="logo" />
+          </header>
+          <>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/appointments" component={Appointments} />
+            <Route exact path="/book" component={BookAppointment} />
+          </>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/pages/Appointments.js
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const Appointments = () => <div>Appointments</div>
+
+export default Appointments
