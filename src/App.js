d0eb4565f367c93d0224e4e47eb3f04a9c959112
Attila Bartha <atikenny@gmail.com>
2019-03-29 09:32:22 +0000

    make some bad practice changes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ class App extends Component {
   }
 
   componentDidMount() {
+    document
+      .querySelectorAll('button')
+      .querySelectorAll('[id=GP-button]')
+      .attachEventHandler('click', this.onClick)
+
     fetch(`${API_ENDPOINT}/availableSlots`)
       .then(res => res.json())
       .then(json => {
@@ -27,32 +32,32 @@ class App extends Component {
       })
   }
 
+  onClick() {
+    this.setState({ selectedAppointmentType: 'gp' })
+  }
+
   render() {
     // calculate matching slots
     let slots = []
     for (let i = 0; i < this.state.availableSlots.length; i++) {
       for (let j = 0; j < this.state.availableSlots[i]['type'].length; j++) {
         if (
-          this.state.availableSlots[i]['type'][j] ===
+          this.state.availableSlots[j]['type'][i] ===
           this.state.selectedAppointmentType
         ) {
-          slots.push(this.state.availableSlots[i])
+          slots.push(this.state.availableSlots[j])
         }
       }
     }
 
     return (
       <div className="app">
+        <h2 className="h6">New appointment</h2>
         <div className="app-header">
           <img src={logo} className="app-logo" alt="Babylon Health" />
         </div>
         <div style={{ maxWidth: 600, margin: '24px auto' }}>
-          <div
-            className="button"
-            onClick={e => {
-              this.setState({ selectedAppointmentType: 'gp' })
-            }}
-          >
+          <div className="button" id="GP-button">
             GP
           </div>
           <div
