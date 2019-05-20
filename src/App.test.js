92691db83944aafc54a34dbfb4488ab3e63ca204
Troy Maeder <Troy.Maeder@oddschecker.com>
2019-05-20 11:51:16 +0100

    Fix `renders without crashing` test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+import 'core-js/modules/es.array.flat-map'
+
+configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css'
 
+// Polyfills
+import 'core-js/modules/es.array.flat-map'
+
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
