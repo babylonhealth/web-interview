70951673ddf966efeb1c1626f2862533047f595a
Troy Maeder <Troy.Maeder@oddschecker.com>
2019-05-20 12:04:48 +0100

    Add test for formatting date to hours and minutes

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import App from './App'
 import 'core-js/modules/es.array.flat-map'
-
-configure({ adapter: new Adapter() })
+import timeFormat from './helpers/time-formatter'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
+})
+
+test('formats date to hours and minutes/HH:mm', () => {
+  expect(timeFormat('2019-08-30T19:21:30.000Z')).toEqual('20:21')
 })
