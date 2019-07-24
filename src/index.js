7b6af1d79c7e16e60732be6a0630aa11a638cd46
Behnaz <behnaz.pourafshari@gmail.com>
2019-07-24 20:16:12 +0100

    add font awesome

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css'
-
+import 'font-awesome/css/font-awesome.min.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
